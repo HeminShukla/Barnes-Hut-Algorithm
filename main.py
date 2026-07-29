e734ce3c5ef0740f9143fe2a04a2a1187c609410
Hemin Shukla <hemin.shukla@gmail.com>
2026-07-29 21:54:44 +0530

    Added a node class with relevant functionality and methods. Added subprograms to draw and generate the quadtree. Next, functionality will be added to have the main body be where the cursor is, as well as calculate s/d and compare to theta continually

--- main.py
+++ main.py
@@ -5,11 +5,27 @@ import simulation
 #Initalise Pygame
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Barnes-Hut")
 
 #First we will add all the bodies
-allBodies = simulation.addBodies(10, 800, 600)
+allBodies = simulation.addBodies(10, 800, 800)
+mainBody = simulation.Body(400, 400, 10) #This is the main body on whom the net force needs to be calculated
+
+#Create the quadtree
+rootNode = simulation.Node(800, 0, 0, allBodies)
+allNodes = [rootNode]
+
+def generateQuadrants(currentNode: simulation.Node) -> list[simulation.Node]:
+    """
+    Recursive function which adds all nodes to the global variable allNodes to be drawn on.
+    """
+    if currentNode.children != []:
+        for child in currentNode.children:
+            allNodes.append(child)
+            generateQuadrants(child)
+
+generateQuadrants(rootNode)
 
 running = True
 while running:
@@ -19,10 +35,19 @@ while running:
 
     screen.fill((0, 0, 0))
 
+
+    #Drawing all of the quadrants in the quadtree onto the graphic
+    for quadrant in allNodes:
+        pygame.draw.rect(screen, (255, 255, 255), (quadrant.left, quadrant.top, quadrant.length, quadrant.length), 1)
+
     for body in allBodies:
-        pygame.draw.circle(screen, (255, 255, 255), (body[0], body[1]), 5)
+            pygame.draw.circle(screen, (211, 211, 211), (body.x, body.y), int(body.mass * 10))
+            pygame.draw.circle(screen, (255, 100, 100), (body.x, body.y), int(body.mass * 9))
 
     
+    #Draw the main body on whom the net force needs to be calculated
+    pygame.draw.circle(screen, (255, 0, 0), (mainBody.x, mainBody.y), mainBody.mass)
+
     pygame.display.flip()
 
 
--- simulation.py
+++ simulation.py
@@ -1,17 +1,65 @@
 import random
 
+class Body():
+    def __init__(self, x, y, mass):
+        self.x = x
+        self.y = y
+        self.mass = mass
+
 class Node():
-    def __init__(self, radius, children, bodies, totalMass, centreOfMass):
-        pass
+    def __init__(self, length, left, top, bodies, totalMass=0, centreOfMass=(0, 0)):
+        """Class representing nodes. 
+        Recursively creates children nodes from the initial node until all required nodes are created. 
+        """
+        self.length = length
+        self.left = left
+        self.top = top
+        self.children = []
+        self.bodies = bodies
+        self.totalMass = totalMass
+        self.centreOfMass = centreOfMass
+
+        if len(self.bodies) > 1: #Only need to create children nodes from here if the number of bodies in this node is greater than 1. Else, the node is small enough
+            #We need to create 4 nodes for the four children from this node
+            #To do that we first need to create the list of bodies that belong in each node from here
+            #The four children nodes will be referred to as nw, ne, sw and se
+            midX = left + length / 2
+            midY = top + length / 2
+            nwBodies = [body for body in self.bodies if body.x < midX and body.y < midY]
+            neBodies = [body for body in self.bodies if body.x > midX and body.y < midY]
+            swBodies = [body for body in self.bodies if body.x < midX and body.y > midY]
+            seBodies = [body for body in self.bodies if body.x > midX and body.y > midY]
+
+            nw = Node(length / 2, self.left, self.top, nwBodies)
+            ne = Node(length/2, self.left + self.length / 2, self.top, neBodies)
+            sw = Node(length / 2, self.left, self.top + self.length / 2, swBodies)
+            se = Node(length/2, self.left + self.length / 2, self.top + self.length / 2, seBodies)
+            self.children = [nw, ne, sw, se]
+
+    def update(self):
+        """
+        Update the node's properties based on its children and bodies, 
+        alllowing for the entire node to be treated as a single body.
+        """
+        #Bodies will be in the format (x, y, mass) 
+        
+        if len(self.bodies) == 0:
+            self.totalMass = 0
+            self.centreOfMass = (0, 0)
+        else:
+            self.totalMass = sum(body.mass for body in self.bodies)
+            self.centreOfMass = (sum(body.x * body.mass for body in self.bodies) / self.totalMass, sum(body.y * body.mass for body in self.bodies) / self.totalMass)
 
 
 
 
-def addBodies(numberOfBodies: int, height: int, width: int) -> list[tuple(int, int, float)]:
+def addBodies(numberOfBodies: int, height: int, width: int) -> list[Body]:
+    """
+    Function that adds a specified number of bodies to the simulation, each with random positions and masses.
+    """
     allBodies = []
     for i in range(numberOfBodies):
-        allBodies.append((random.randint(0, height), random.randint(0, width), random.random()))
-
-    print ("BODIES MADE")
+        allBodies.append(Body(random.randint(0, height), random.randint(0, width), random.random()))
 
     return allBodies
+
