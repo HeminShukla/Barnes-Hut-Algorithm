fc8d85769e80ec26b22a65ed76568c3c9323cf84
Hemin Shukla <hemin.shukla@gmail.com>
2026-09-15 23:43:22 +0530

    Added ability to generate a cluster-based 'space', changed how the net force and all forces on the main body is calculated, and added funcionality to allow for all bodies to use the value of theta to experience the force exerted on them by all other bodies intelligently.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pygame
 import pygame_widgets
 from pygame_widgets.slider import Slider
 from pygame_widgets.button import Button
-from pygame_widgets.textbox import TextBox
 import simulation
 import math
 
@@ -16,7 +15,7 @@ screen = pygame.display.set_mode((1400, 800))
 pygame.display.set_caption("Barnes-Hut")
 
 #Set up the slider and the display for the textbox
-thetaSlider = Slider(screen, 900, 100, 400, 50, min=0, max=2, step=0.05, colour=(211, 211, 211), handleColour=(64, 64, 64), initialValue=1.5)
+thetaSlider = Slider(screen, 900, 100, 400, 50, min=0, max=2, step=0.05, colour=(211, 211, 211), handleColour=(64, 64, 64), initialValue=1.0)
 font = pygame.font.SysFont(None, 50)
 
 #Set up all of the buttons to toggle different features on and off
@@ -46,10 +45,8 @@ centresOfMassButton = Button(screen, 900, 700, 400, 50, text="Show Centres of Ma
 
 #First we will add all the bodies
 allBodies = simulation.addBodies(100, 800, 800)
+#allBodies = simulation.addCluster(50, 800, 800, 5, 10)
 mainBody = simulation.Body(400, 400, 2) #This is the main body on whom the net force needs to be calculated
-
-#Create the quadtree
-rootNode = simulation.Node(800, 0, 0, allBodies)
 
 running = True
 while running:
@@ -59,7 +56,7 @@ while running:
             running = False
 
     screen.fill((0, 0, 0))
-
+    
     #Creating the slider allowing for theta to be changed by the user
     simulation.theta = round(thetaSlider.getValue(), 2)
     sliderOutput = font.render(f"{simulation.theta:.2f}", True, (255, 255, 255))
@@ -67,6 +64,8 @@ while running:
     screen.blit(thetaLabel, (1100, 50))
     screen.blit(sliderOutput,(1075, 200))
 
+    #Create the quadtree, needs to be done every frame since the bodies are going to be moving around, and the quadtree needs to be updated to reflect this
+    rootNode = simulation.Node(800, 0, 0, allBodies)
 
     #Draw the main body on whom the net force needs to be calculated
     currentMousePosition = pygame.mouse.get_pos()
@@ -82,30 +81,89 @@ while running:
 
     #Drawing all the individual bodies in
     for body in allBodies:
-            pygame.draw.circle(screen, (211, 211, 211), (body.x, body.y), int(body.mass * 10))
-            pygame.draw.circle(screen, (255, 255, 255), (body.x, body.y), int(body.mass * 9))
+            if not simulation.outOfBounds(body, 800, 800):
+                pygame.draw.circle(screen, (211, 211, 211), (body.x, body.y), int(body.mass * 10))
+                pygame.draw.circle(screen, (255, 255, 255), (body.x, body.y), int(body.mass * 9))
 
+    #Calculating the total force acting on the main body, and drawing lines to show the forces acting on it
     totalForce = (0, 0) #This will be a tuple, representing the total force in the x and y direction as a vector
+
+    forceOnBodies = {} #This will be a dictionary mapping each body to the total force acting on it
+    for currentBody in allBodies:
+        forceOnBodies[currentBody] = (0, 0) #This will be a tuple, representing the total force in the x and y direction as a vector
+        
     for sourceOfForce in simulation.allNodes:
         #Each value in allNodes shows a source of a force that will be acting on the main body
         #So we want to constantly draw a line from that point where the force is acting to the main body
         try:
-            if not centresOfMassToggle:
-                pygame.draw.circle(
-                                    screen,
-                                    (0, 255, 0),
-                                    (int(sourceOfForce.centreOfMass[0]), int(sourceOfForce.centreOfMass[1])),
-                                    3
-                                )
-            currentForce = simulation.calculateForce(sourceOfForce, mainBody) * 10 ** 13 #We multiply by 10^13 almost arbitratily, to ensure it is visible to the user 
-            angle = math.atan2(sourceOfForce.centreOfMass[1] - currentMousePosition[1], sourceOfForce.centreOfMass[0]-currentMousePosition[0])
-            distance = math.hypot(sourceOfForce.centreOfMass[0] - currentMousePosition[0], sourceOfForce.centreOfMass[1]-currentMousePosition[1])
-            totalForce = (totalForce[0] + currentForce * math.cos(angle), totalForce[1] + currentForce * math.sin(angle))
-            if not allForcesToggle:
-                pygame.draw.line(screen, (64, 64, 150),currentMousePosition, sourceOfForce.centreOfMass)
-        except MemoryError:
-            pass #This is because we need to multiple by 10^13 to make the forces visible, but this can cause a memory error if the force is too large
+            if set.intersection(set(sourceOfForce.children), set(simulation.allNodes)) == set()  and sourceOfForce.bodies != [] and sourceOfForce != rootNode: 
+                #Checking that no node whose children are already being counted is also being counted and ensuring the root node is not counted twice
+                if not centresOfMassToggle:
+                    pygame.draw.circle(
+                                        screen,
+                                        (0, 255, 0),
+                                        (int(sourceOfForce.centreOfMass[0]), int(sourceOfForce.centreOfMass[1])),
+                                        3
+                                    )
+                currentForce = simulation.calculateForce(sourceOfForce, mainBody) * 10 ** 13 #We multiply by 10^13 almost arbitratily, to ensure it is visible to the user 
+                angle = math.atan2(sourceOfForce.centreOfMass[1] - currentMousePosition[1], sourceOfForce.centreOfMass[0]-currentMousePosition[0])
+                distance = math.hypot(sourceOfForce.centreOfMass[0] - currentMousePosition[0], sourceOfForce.centreOfMass[1]-currentMousePosition[1])
+                totalForce = (totalForce[0] + currentForce * math.cos(angle), totalForce[1] + currentForce * math.sin(angle))
+                if not allForcesToggle and sourceOfForce.totalMass != 0:
+                    pygame.draw.line(screen, (64, 64, 150),currentMousePosition, sourceOfForce.centreOfMass)
 
+        except MemoryError:
+            pass #This is because we need to multiply by 10^13 to make the forces visible, but this can cause a memory error if the force is too large
+
+    #Now we will calculate the total force acting on each body, and store it in the dictionary forceOnBodies            
+    for currentBody in allBodies:
+        if not simulation.outOfBounds(currentBody, 800, 800): 
+            #We do not want to calculate the force acting on a body from itself, since this is not physically possible
+            simulation.generateQuadrants(rootNode, (currentBody.x, currentBody.y))
+            for sourceOfForce in simulation.allNodes:
+                if currentBody not in sourceOfForce.bodies:
+                    distanceOnBody = math.hypot(sourceOfForce.centreOfMass[0] - currentBody.x, sourceOfForce.centreOfMass[1]-currentBody.y)
+                    if distanceOnBody > 0.3: #This is because if they get too close, they will simply collide, we can treat this as them merging them in a sense, since they will now just not react  to each other and stay close
+                        forceOnBody = simulation.calculateForce(sourceOfForce, currentBody) #We do not times this by 10^13, since we want to see the actual forces acting on the bodies, not just a visual representation of them
+                        angleOnBody = math.atan2(sourceOfForce.centreOfMass[1] - currentBody.y, sourceOfForce.centreOfMass[0]-currentBody.x)
+                        totalForceOnBody = (forceOnBody * math.cos(angleOnBody), forceOnBody * math.sin(angleOnBody))
+                        forceOnBodies[currentBody] = (forceOnBodies[currentBody][0] + totalForceOnBody[0], forceOnBodies[currentBody][1] + totalForceOnBody[1])
+
+        
+
+    #Now to move the bodies to reflect this movement
+    for body in allBodies:
+        #Firstly the body's position will be updated, using t he speed
+        magnitude = 10**9
+        acceleration = (magnitude * forceOnBodies[body][0] / body.mass, magnitude * forceOnBodies[body][1] / body.mass)
+        #The velocity will be updated, derived from v = u + at, assuming t = 1 (per frame)
+        body.speed = (body.speed[0] + acceleration[0], body.speed[1] + acceleration[1])
+        body.x += body.speed[0]
+        body.y += body.speed[1]
+        
+    
+
+    """for body in allBodies:
+        found = False
+        for node in simulation.allNodes:
+            if body in node.bodies:
+                found = True
+                break
+        if found == False:
+            pygame.draw.circle(screen, (255, 0, 0), (body.x, body.y), int(body.mass * 10))
+            pygame.draw.circle(screen, (255, 255, 255), (body.x, body.y), int(body.mass * 9))
+
+    allNodeCheck = [rootNode]
+    simulation.allNodeCheckFunc(rootNode, allNodeCheck)
+    neglectedNodes = (neglectedNode for neglectedNode in allNodeCheck if neglectedNode not in simulation.allNodes)
+    for neglectedNode in neglectedNodes:
+        pygame.draw.circle(
+                            screen,
+                            (255, 0, 0),
+                            (int(neglectedNode.centreOfMass[0]+1), int(neglectedNode.centreOfMass[1]+1)),
+                            3
+                        )"""
+    
     #Now to draw the total force acting on the main body
     #This will be an arrow, and will be a different colour to differentiate it from the other lines showing forces that are influencing the main body
     tip = (currentMousePosition[0] + totalForce[0], currentMousePosition[1] + totalForce[1])
@@ -124,8 +182,6 @@ while running:
         pygame.draw.polygon(screen, (255, 0, 0), (tip, left, right))
 
     
-
-
     pygame_widgets.update(events)
     pygame.display.flip()
 
--- simulation.py
+++ simulation.py
@@ -2,10 +2,11 @@ import random
 import math
 
 class Body():
-    def __init__(self, x, y, mass):
+    def __init__(self, x, y, mass, speed=(0,0)):
         self.x = x
         self.y = y
         self.mass = mass
+        self.speed = speed
 
 class Node():
     def __init__(self, length, left, top, bodies: list[Body], totalMass=0, centreOfMass=(0, 0)):
@@ -26,10 +27,10 @@ class Node():
             #The four children nodes will be referred to as nw, ne, sw and se
             midX = left + length / 2
             midY = top + length / 2
-            nwBodies = [body for body in self.bodies if body.x < midX and body.y < midY]
-            neBodies = [body for body in self.bodies if body.x > midX and body.y < midY]
-            swBodies = [body for body in self.bodies if body.x < midX and body.y > midY]
-            seBodies = [body for body in self.bodies if body.x > midX and body.y > midY]
+            nwBodies = [body for body in self.bodies if 0 <= body.x <= midX and 0 <= body.y <= midY]
+            neBodies = [body for body in self.bodies if 800 >= body.x >= midX and 0 <= body.y <= midY]
+            swBodies = [body for body in self.bodies if 0 <= body.x <= midX and 800 >= body.y >= midY]
+            seBodies = [body for body in self.bodies if 800 >= body.x >= midX and 800 >= body.y >= midY]
 
             nw = Node(length / 2, self.left, self.top, nwBodies)
             ne = Node(length/2, self.left + self.length / 2, self.top, neBodies)
@@ -40,13 +41,13 @@ class Node():
     def update(self):
         """
         Update the node's properties based on its children and bodies, 
-        alllowing for the entire node to be treated as a single body.
+        allowing for the entire node to be treated as a single body.
         """
         #Bodies will be in the format (x, y, mass) 
         
         if len(self.bodies) == 0:
             self.totalMass = 0
-            self.centreOfMass = (0, 0)
+            self.centreOfMass = (self.left,self.top)
         else:
             self.totalMass = sum(body.mass for body in self.bodies)
             self.centreOfMass = (sum(body.x * body.mass for body in self.bodies) / self.totalMass, sum(body.y * body.mass for body in self.bodies) / self.totalMass)
@@ -60,9 +61,37 @@ def addBodies(numberOfBodies: int, height: int, width: int) -> list[Body]:
     """
     allBodies = []
     for i in range(numberOfBodies):
-        allBodies.append(Body(random.randint(0, height), random.randint(0, width), random.random()))
+        allBodies.append(Body(random.randint(0, width), random.randint(0, height), 0.3))
 
     return allBodies
+
+def addCluster(numberOfBodies: int, height: int, width: int, numberOfClusters: int, clusterRadius: int) ->  list[Body]:
+    """
+    Function that adds a specified number of bodies to the simulation, within a specified number of clusters
+    """
+    allBodies = []
+    clusterList = []
+    for i in range(numberOfClusters): #Create the coordinates for the clusters around which the bodies will form
+        clusterList.append((random.randint(0, width), random.randint(0, height)))
+
+    #Now for each generated body, we will generate it a random distance away from a random cluster, at a random angle
+    for i in range(numberOfBodies):
+        currentCluster = random.choice(clusterList)
+        distance = random.randint(0, clusterRadius)
+        angle = random.randint(0, 360)
+        bodyCoordinates = (currentCluster[0] + distance * math.cos(angle), currentCluster[1] + distance * math.sin(angle))
+        allBodies.append(Body(bodyCoordinates[0], bodyCoordinates[1], 0.3))
+
+    return allBodies
+
+
+def allNodeCheckFunc(rootNode: Node, nodesToAdd: list[Node]) -> None:
+    if rootNode.children != []:
+        for child in rootNode.children:
+            allNodeCheckFunc(child, nodesToAdd)
+    else:
+        nodesToAdd.append(rootNode)
+
 
 allNodes = [] #Global variable to store all nodes in the quadtree for drawing purposes
 theta = 1 #Global variable to store the value of theta, which will be updated by the user through the main program
@@ -73,8 +102,9 @@ def generateQuadrants(currentNode: Node, currentPos: tuple[int, int]) -> list[No
     currentNode.update()
     if currentNode.children != [] and calculateQuotient(currentNode, currentPos) > theta:
         for child in currentNode.children:
-            allNodes.append(child)
-            generateQuadrants(child, currentPos)
+            if child not in allNodes:
+                allNodes.append(child)
+                generateQuadrants(child, currentPos)
 
 
 def calculateQuotient(currentNode: Node, currentPos: tuple[int, int]) -> float:
@@ -90,8 +120,26 @@ def calculateQuotient(currentNode: Node, currentPos: tuple[int, int]) -> float:
 
 def calculateForce(currentNode: Node, body: Body) -> float:
     try:
-        distanceToSource = math.hypot(body.x - currentNode.centreOfMass[0], body.y - currentNode.centreOfMass[1])
+        distanceToSource = math.hypot(abs(body.x - currentNode.centreOfMass[0]), abs(body.y - currentNode.centreOfMass[1]))
         force = 6.67 * (10 ** -11) * body.mass * currentNode.totalMass / (distanceToSource ** 2)
         return force
     except (ZeroDivisionError, MemoryError):
         return 0.0
+
+def calculateBodyForce(bodyOne: Body, bodyTwo: Node) -> float:
+    try:
+        distanceToSource = math.hypot(abs(bodyOne.x - bodyTwo.centreOfMass[0]), abs(bodyOne.y - bodyTwo.centreOfMass[1]))
+        force = 6.67 * (10 ** -11) * bodyOne.mass * bodyTwo.totalMass / (distanceToSource ** 2)
+        return force
+    except (ZeroDivisionError, MemoryError):
+        return 0.0
+
+
+def outOfBounds(body: Body, width: int, height: int) -> bool:
+    """
+    Function that checks if a body is out of bounds of the simulation area.
+    """
+    if body.x < 0 or body.x >= width or body.y < 0 or body.y >= height:
+        return True
+    else:
+        return False
